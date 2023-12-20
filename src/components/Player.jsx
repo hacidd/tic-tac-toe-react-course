2c066997c004a8b4ab55bf4713acc3d499ccf4be
hacidd <radoslawef@gmail.com>
2023-12-20 10:33:29 +0100

    updating state based on old state

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,13 +1,25 @@
+import { useState } from 'react'
 
+export default function Player({ name, symbol }) {
+	const [isEditing, setIsEditing] = useState(false)
 
-export default function Player({name, symbol}) {
+	function handleEditClick() {
+		setIsEditing((editing) => !editing)
+	}
+	let playerName = <span className='player-name'>{name}</span>
+    let btnCaption = 'Edit'
+
+	if (isEditing) {
+		playerName = <input type='text' required value={name} />
+        btnCaption = 'Save'
+	}
 	return (
 		<li>
 			<span className='player'>
-				<span className='player-name'>{name}</span>
+				{playerName}
 				<span className='player-symbol'>{symbol}</span>
 			</span>
-			<button>Edit</button>
+			<button onClick={handleEditClick}>{btnCaption}</button>
 		</li>
 	)
 }
