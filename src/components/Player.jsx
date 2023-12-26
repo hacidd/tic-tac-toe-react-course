6968fea14fcdde19d12514682068caf9496b4f74
hacidd <radoslawef@gmail.com>
2023-12-26 10:21:51 +0100

    final touch

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,24 +1,27 @@
 import { useState } from 'react'
 
-export default function Player({ initialName, symbol, isActive }) {
-    const [playerName, setPlayerName] = useState(initialName);
+export default function Player({ initialName, symbol, isActive, onChangeName }) {
+	const [playerName, setPlayerName] = useState(initialName)
 	const [isEditing, setIsEditing] = useState(false)
 
 	function handleEditClick() {
-		setIsEditing((editing) => !editing)
+		setIsEditing(editing => !editing)
+
+		if (isEditing) {
+			onChangeName(symbol, playerName)
+		}
 	}
 
-function handleChange (event) {
-    setPlayerName(event.target.value);
-
-}
+	function handleChange(event) {
+		setPlayerName(event.target.value)
+	}
 
 	let editablePlayerName = <span className='player-name'>{playerName}</span>
-    let btnCaption = 'Edit'
+	let btnCaption = 'Edit'
 
 	if (isEditing) {
 		editablePlayerName = <input type='text' required defaultValue={playerName} onChange={handleChange} />
-        btnCaption = 'Save'
+		btnCaption = 'Save'
 	}
 	return (
 		<li className={isActive ? 'active' : undefined}>
